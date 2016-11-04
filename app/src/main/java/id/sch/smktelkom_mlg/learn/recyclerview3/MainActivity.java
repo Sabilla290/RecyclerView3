a110cc2a3aa8c844a3427573d88f2d3bbe50d0df
Sabilla <Sabilla290@gmail.com>
2016-11-04 06:58:19 -0700

    RecyclerView View Detail

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -1,5 +1,7 @@
 package id.sch.smktelkom_mlg.learn.recyclerview3;
 
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
@@ -19,8 +21,9 @@ import java.util.ArrayList;
 import id.sch.smktelkom_mlg.learn.recyclerview3.adapter.HotelAdapter;
 import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HotelAdapter.IHotelAdapter {
 
+    public String HOTEL;
     ArrayList<Hotel>mList = new ArrayList<>();
     HotelAdapter mAdapter;
     @Override
@@ -31,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        mAdapter = new HotelAdapter(mList);
+        mAdapter = new HotelAdapter(this, mList);
         recyclerView.setAdapter(mAdapter);
         fillData();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -49,18 +52,26 @@ public class MainActivity extends AppCompatActivity {
 
     private void fillData() {
         Resources resources = getResources();
+        String[] arJudul = resources.getStringArray(R.array.places);
         String [] arDeskripsi = resources.getStringArray(R.array.place_desc);
-        String [] arJudul = resources.getStringArray(R.array.places);
+        String [] arDetail = resources.getStringArray(R.array.place_details);
+        String [] arLokasi = resources.getStringArray(R.array.place_locations);
         TypedArray a = resources.obtainTypedArray(R.array.places_picture);
-        Drawable [] arFoto = new Drawable[a.length()];
+        String [] arFoto = new String [a.length()];
         for (int i = 0; i<arFoto.length; i++)
         {
-            arFoto[i] = a.getDrawable(i);
+            int id = a.getResourceId(i,0);
+
+            arFoto[i] = ContentResolver.SCHEME_ANDROID_RESOURCE+"://"
+                    +resources.getResourcePackageName(id)+'/'
+                    +resources.getResourceTypeName(id)+'/'
+                    +resources.getResourceEntryName(id);
         }
         a.recycle();
         for (int i = 0;i<arJudul.length;i++)
         {
-            mList.add(new Hotel(arJudul[i],arDeskripsi[i],arFoto[i]));
+            mList.add(new Hotel(arJudul[i],arDeskripsi[i],
+                    arDetail[i],arLokasi[i],arFoto[i]));
         }
         mAdapter.notifyDataSetChanged();
     }
@@ -71,7 +82,6 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -85,5 +95,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void doClick(int pos) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(HOTEL,mList.get(pos));
+        startActivity(intent);
     }
 }
